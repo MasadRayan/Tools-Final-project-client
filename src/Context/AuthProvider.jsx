b9ff20524419fae430d7971e473e73a5637ed2b6
MasadRayan <masadrayan2002@gmail.com>
2025-12-15 21:38:01 +0600

    Login user, createUser added

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../Firebase/firebase.init';
 
 const AuthProvider = ({children}) => {
-    const authData = {
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState(null);
 
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const authData = {
+        loading,
+        user,
+        loginUser,
+        createUser,
+        
     }
     return <AuthContext value={authData}>
         {children}
