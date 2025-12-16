de4c0415a60301665d19ed56ad717cd3e7c06652
MasadRayan <masadrayan2002@gmail.com>
2025-12-16 15:50:51 +0600

    StatusSection created and react countUp added

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import HeroSection from './Components/HeroSection';
 import FAQ from './Components/FAQ';
 import NewsletterSection from './Components/NewsletterSection';
+import StatsSection from './Components/StatsSection';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSection></HeroSection>
+            <StatsSection></StatsSection>
             <FAQ></FAQ>
             <NewsletterSection></NewsletterSection>
         </div>
