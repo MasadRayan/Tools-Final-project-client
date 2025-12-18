f376bc29141148d73ddba7718873782152e7106f
MasadRayan <masadrayan2002@gmail.com>
2025-12-18 19:37:55 +0600

    Added Private Route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navigate, useLocation, useNavigate } from 'react-router';
+import useAuth from '../Hooks/useAuth';
+import LoadingSpinner from '../Components/LoadingSpinner/LoadingSpinner';
+
+const PrivateRoute = ({children}) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const {user, loading} = useAuth();
+
+    if (loading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    if (user) {
+        return children
+    }
+
+    return <Navigate to="/login" state={{from: location.pathname}} ></Navigate>
+};
+
+export default PrivateRoute;
