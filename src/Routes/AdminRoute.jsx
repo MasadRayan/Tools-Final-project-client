07132791e603128be6b319fba3729c5405c28991
MasadRayan <masadrayan2002@gmail.com>
2025-12-19 22:36:41 +0600

    Added admin Route

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import useAuth from '../Hooks/useAuth';
+import useUserRole from '../Hooks/getUserRole';
+import LoadingSpinner from '../Components/LoadingSpinner/LoadingSpinner';
+import { Navigate } from 'react-router';
+
+const AdminRoute = ({ children }) => {
+
+    const {user, loading} = useAuth();
+    const {role, roleLoading} = useUserRole();
+
+    if (loading || roleLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    if (!user || role !== "admin") {
+        return <Navigate to={'/forbidden'}></Navigate>
+    }
+
+    return children;
+};
+
+export default AdminRoute;
