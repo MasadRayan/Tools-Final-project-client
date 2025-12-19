094757b53981e2c07df62b1781ef9ab762be7f56
MasadRayan <masadrayan2002@gmail.com>
2025-12-19 22:28:13 +0600

    User Route added

---
+++ src/Routes/UserRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import useUserRole from '../Hooks/getUserRole';
+import useAuth from '../Hooks/useAuth';
+import { Navigate } from 'react-router';
+import LoadingSpinner from '../Components/LoadingSpinner/LoadingSpinner';
+
+const UserRoute = ({ children }) => {
+    const {role, roleLoading} = useUserRole();
+    const {user, loading} = useAuth();
+
+    if (!user || role !== user) {
+        return <Navigate to={'/forbidden'}></Navigate>
+    }
+
+    if (loading || roleLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+
+    return children;
+};
+
+export default UserRoute;
